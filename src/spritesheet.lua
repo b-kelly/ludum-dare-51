dbcd7e972df23c09e2c210bd77066e23e1564a79
Ben Kelly <b-kelly@users.noreply.github.com>
2022-10-01 12:59:14 -0400

    fix texture cycling

--- src/spritesheet.lua
+++ src/spritesheet.lua
@@ -1,9 +1,9 @@
 function loadSpritesheet(img, xCount, yCount, spriteSize)
   local arr = {}
   
-  for x=0,xCount do
+  for x=0,xCount-1 do
     local xCoord = x * spriteSize
-    for y=0,yCount do
+    for y=0,yCount-1 do
       local yCoord = y * spriteSize
       local quad = love.graphics.newQuad(xCoord, yCoord, spriteSize, spriteSize, img)
       table.insert(arr, quad)
