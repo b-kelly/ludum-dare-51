e8415c878f79134351768adf991803eab05fa712
Ben Kelly <b-kelly@users.noreply.github.com>
2022-10-01 10:54:33 -0400

    add in reference grid bg

--- src/ui.lua
+++ src/ui.lua
@@ -2,10 +2,14 @@ local bg
 
 function loadUI()
     bg = love.graphics.newImage('assets/background.png')
+    referenceArea = love.graphics.newImage('assets/referenceBackground.png')
+    referenceAreaGrid = love.graphics.newImage('assets/referenceBackground_grid.png')
 end
 
 function drawUI()
     love.graphics.draw(bg, 0, 0)
+    love.graphics.draw(referenceArea, 0, 0)
+    love.graphics.draw(referenceAreaGrid, 0, 0)
 end
 
 function drawDebug(data)
