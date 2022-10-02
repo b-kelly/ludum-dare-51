8b4347264dda66189c48c569a9c6e875e1d8b4b4
b-kelly <b-kelly@users.noreply.github.com>
2022-10-02 01:05:18 +0000

    Deploying to gh-pages from @ b-kelly/ludum-dare-51@76a50dbdfe6d82589ad7475c1f6c04654ab6e423 🚀

--- game.js
+++ game.js
@@ -284,6 +284,6 @@ Module.expectedDataFileDownloads++;
     }
 
   }
-  loadPackage({"package_uuid":"498d46b7-4026-436b-b8c9-bccfbb3de831","remote_package_size":187059,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":187059,"audio":false}]});
+  loadPackage({"package_uuid":"b679237e-9dab-442e-b6ad-3e93d1e675b8","remote_package_size":187103,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":187103,"audio":false}]});
 
 })();
