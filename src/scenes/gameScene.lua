bce60d11e14f84eba0a978e192e9dce42b47f485
Ben Kelly <b-kelly@users.noreply.github.com>
2022-10-01 22:45:43 -0400

    drop ui.lua

--- src/scenes/gameScene.lua
+++ src/scenes/gameScene.lua
@@ -1,7 +1,81 @@
-require "ui"
-
 local SG = {}
 SG.__index = SG
+
+local bg
+
+local function newButton(buttonImg, buttonX, buttonY)
+  return {
+    img = buttonImg,
+    x1 = buttonX,
+    y1 = buttonY,
+    x2 = buttonX + buttonImg:getWidth(),
+    y2 = buttonY + buttonImg:getHeight()
+  }
+end
+
+local function loadUI()
+    bg = love.graphics.newImage('assets/background.png')
+    referenceArea = love.graphics.newImage('assets/referenceBackground.png')
+    referenceAreaGrid = love.graphics.newImage('assets/referenceBackground_grid.png')
+    workspaceGrid = love.graphics.newImage('assets/workingCanvas_grid.png')
+    undoButton = love.graphics.newImage('assets/undoButton.png')
+    rotateButton = love.graphics.newImage('assets/rotateButton.png')
+    clearButton = love.graphics.newImage('assets/clearButton.png')
+    buttons = {}
+    local canvasButtonX = 160
+    buttons[1] = newButton(rotateButton, canvasButtonX, 260)
+    buttons[2] = newButton(undoButton, canvasButtonX, 330)
+    buttons[3] = newButton(clearButton, canvasButtonX, 400)
+end
+
+local function drawDebug(scorer, texture, sprite, mx, my)
+    love.graphics.push()
+    love.graphics.scale(0.5, 0.5)
+    scorer:drawDebug(texture, sprite)
+    love.graphics.pop()
+    love.graphics.printf(mx .. ", " .. my, mx+10, my, 40, "left")
+end
+
+local function loadBagLocations()
+  bagLocations = {}
+  local topRowY = 82
+  local topRowBottomY = 185
+  local bottomRowY = 195
+  local bottomRowBottomY = 275
+  -- { top left x, top left y, bottom right x, bottom right y
+  bagLocations[1] = {x1 = 276, y1 = topRowY, x2 = 360, y2 = topRowBottomY}
+  bagLocations[2] = {x1 = 375, y1 = topRowY, x2 = 475, y2 = topRowBottomY}
+  bagLocations[3] = {x1 = 487, y1 = topRowY, x2 = 560, y2 = topRowBottomY}
+  bagLocations[4] = {x1 = 570, y1 = topRowY, x2 = 645, y2 = topRowBottomY}
+  bagLocations[5] = {x1 = 650, y1 = topRowY, x2 = 740, y2 = topRowBottomY}
+  bagLocations[6] = {x1 = 272, y1 = bottomRowY, x2 = 351, y2 = bottomRowBottomY}
+  bagLocations[7] = {x1 = 365, y1 = bottomRowY, x2 = 437, y2 = bottomRowBottomY}
+  bagLocations[8] = {x1 = 450, y1 = bottomRowY, x2 = 520, y2 = bottomRowBottomY}
+  bagLocations[9] = {x1 = 530, y1 = bottomRowY, x2 = 627, y2 = bottomRowBottomY}
+  bagLocations[10] = {x1 = 645, y1 = bottomRowY, x2 = 736, y2 = bottomRowBottomY}
+end
+
+local function drawSecondsGauge(points)
+  -- TODO some sort of gauge/graphic for seconds?
+  love.graphics.setColor(0, 0, 0)
+  love.graphics.print(points["spent"] .. "/" .. points["max"], 85, 415)
+  love.graphics.setColor(255, 255, 255)
+end
+
+local function drawUI(fn, points)
+    love.graphics.draw(bg, 0, 0)
+    love.graphics.draw(referenceArea, 100, 80)
+    fn()
+    love.graphics.draw(referenceAreaGrid, 110, 90)
+    love.graphics.draw(workspaceGrid, 260, 314)
+
+    drawSecondsGauge(points)
+
+    for i=1, #buttons do
+      local b = buttons[i]
+      love.graphics.draw(b.img, b.x1, b.y1)
+    end
+end
 
 local function nextRound(reference, workspace, gameState, scorer)
   local data = reference:getData()
