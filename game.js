a0b755846b807b8b574a198e9a4abe5f362b670c
kristinalustig <kristina.lustig@gmail.com>
2022-10-02 20:22:29 +0000

    Deploying to gh-pages from @ b-kelly/ludum-dare-51@74fdbb1721968c6aee052e26ec0f6a6a531e0287 🚀

--- game.js
+++ game.js
@@ -284,6 +284,6 @@ Module.expectedDataFileDownloads++;
     }
 
   }
-  loadPackage({"package_uuid":"3e55819f-0183-4946-a067-e27306bbe4c4","remote_package_size":2797722,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":2797722,"audio":false}]});
+  loadPackage({"package_uuid":"0b380f89-963f-4461-bff8-e4b01e7d4604","remote_package_size":2797722,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":2797722,"audio":false}]});
 
 })();
