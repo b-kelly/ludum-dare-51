139879bf95994f4f49584f2b250ad03a6ae1b83f
kristinalustig <kristina.lustig@gmail.com>
2022-10-03 11:26:39 -0400

    Done with strings!

--- src/strings.lua
+++ src/strings.lua
@@ -13,14 +13,14 @@ local conversations = {
     {"Hey Gobby, ever wonder what's outside this forest?", "Sometimes I wonder what's outside this stump... I used to remember...", nil, "Yes yes yes! Keep going, contraption! You can do it!"},
     {"Hey, wanna switch spots for a bit?", "Would I ever! Wow, Gorbo, thanks, I'm getting a bit cramped in here.", nil, "Coming right up! Er, maybe next time, Gobby."},
     {"Get your objects here! Ready in 10 or it's free!", "You know, 10 seconds isn't a very long time to get this done. Can't I have 30?", nil, "Can do, can do. This contraption is SO fast, just you watch!"},
-    {"Gorbo.", "Gobby.", nil, "Our contraption can do that!."},
-    {"Gorbo.", "Gobby.", nil, "We'll get on that right away."},
-    {"Gorbo.", "Gobby.", nil, "Absolutely!."},
-    {"Gorbo.", "Gobby.", nil, "Yes, I think we can manage that."},
-    {"Gorbo.", "Gobby.", nil, "Of course."},
-    {"Gorbo.", "Gobby.", nil, "Sure can."},
-    {"Gorbo.", "Gobby.", nil, "It'll be ready in two shakes of a goblin's ears."},
-    {"Gorbo.", "Gobby.", nil, "Yep, it'll be ready in 10 or my name ain't Gorbo."}
+    {"You know, I think business is starting to pick up.", "That's good news! Maybe we can upgrade to an actual contraption someday, instead of just me in a stump.", nil, "Yeah yeah, later, Gobby. Our contraption can do that!"},
+    {"Treat yourself to a freshly copied object, any object you'd like!", "Hopefully one that won't like, bite me or poison me or something.", nil, "We'll get on that right away. Gobby, you'll be fine."},
+    {"Objects copied faster than any incredible copying contraption in the forest, guaranteed!", "...are there really other copying contraptions out there? Maybe I could compare notes with them.", nil, "Absolutely! And Gobby, no, they can't learn our secrets!"},
+    {"I'm tired of standing here all day, ugh.", "I mean, we could switch spots...", nil, "Yes, I think we can manage that. What, no Gobby, not switching. We can manage making that."},
+    {"You know what would be SO cool to copy? A pony.", "Well, Gorbo, I don't know if a pony would fit in here with me...", nil, "Of course. ...perhaps you'd like a pony as well? No? No matter."},
+    {"Hey Gobby? Why do we call this copying? It's more like 'conjuring' or 'creating'.", "Well, I think copying is probably a bit catchier, right?", nil, "Sure can. Let's copy away!"},
+    {"Boooored. Getcha things here, objects of whatever kind, get 'em here...", "You gotta be more enthusiastic than that!", nil, "It'll be ready in two shakes of a goblin's ears."},
+    {"Hey Gobby, what's your favorite puppet show?", "I like spooky puppet shows like 'Autumn Ghost' and 'I Know What You Did Last Solstice In The Apothecary'.", nil, "Yep, it'll be ready in 10 or my name ain't Gorbo. Gobby, I haven't seen either of those!"}
 }
 
 --I copied some of these twice across ratings because they apply to more than one.
@@ -73,68 +73,68 @@ local requests = {
     },
     {
         "Oh Gorbo I'm so glad to see you. I forgot to buy my daughter a gift for Solstice and she REALLY wanted this one doll, and it's all sold out at the toymaker's! Is there any way you could make one just like it?",
-        "test dolly 2",
+        "Hey Gorbo, I was actually looking for Gobby? Because he forgot that today is his daughter's birthday? And he better make a doll for little Gobbina ASAP?",
     },
     {
-        "test frog 1",
-        "test frog 2",
+        "Hi! I was working on a quick little potion and realized I am ALL out of frogs. Got any frogs in there? Can you make me one?",
+        "Hi there. I need a new best friend. And I would like that friend to be a frog. Can I have a frog please? Like, a really nice one who likes to play board games?",
     },
     {
-        "test duck 1",
-        "test duck 2",
+        "Hello. My pond back home is sorely lacking something and I *think* what it's lacking is a duck. Can you make me a duck?",
+        "Ooooh, you can make anything? Ha-ha, what about a duck? That would be funny. A duck! Quack quack! Ha-ha-ha. A duck. A duck?",
     },
     {
-        "test hatchet 1",
-        "test hatchet 2",
+        "Greetings! I need a tool that is good for chopping trees, I'm looking to get into lumberjacking. Maybe like... a tree-chopper? Is that what it's called? Is that possible?",
+        "Hullo. I was chopping things and accidentally tried to chop a rock, and it didn't work, and now my axe is broken. Could you make me a new one?",
     },
     {
-        "test hourglass 1",
-        "test hourglass 2",
+        "Oh, I'm so glad you're open! I keep trying to bake cookies and keep burning them because I don't have a way to time them! Could you make me an hourglass?",
+        "Hi there. I'm trying to cultivate a sort of mysterious, vaguely menacing vibe. I think having an hourglass would help. What do you think? You got an hourglass in there?",
     },
     {
-        "test glasses 1",
-        "test glasses 2",
+        "Gorbo, is that you? I can't quite see. My old glasses broke. Can you make me new glasses? If that IS you, Gorbo. If not, hello stranger, could YOU make me glasses?",
+        "Hello! I hear glasses are very 'in' right now. I need the 'in'-nest glasses possible, can you accommodate me?",
     },
     {
-        "test boots 1",
-        "test boots 2",
+        "Hi Gorbo! I am SO tired of walking everywhere. I mean, one step, then another one, then another one... who has the time?! How about some winged boots?",
+        "Greetings and salutations. I'm running a race soon and would REALLY like to win, so can you make some winged boots? I'm pretty sure that's not cheating.",
     },
     {
-        "test anvil 1",
-        "test anvil 2",
+        "Ugh, Gorbo, you won't believe what happened. Garbonzo decided he'd use my forge and he split my anvil in two! Can you believe the nerve of that guy? Now I need a new Anvil. Perhaps you could make me one?",
+        "Oh, hi, is this the amazing copier? I wanted to get into blacksmithing and *apparently* just smashing metal on the floor doesn't work. I need to smash it on an *anvil*. Do you make those here?",
     },
     {
-        "test crab 1",
-        "test crab 2",
+        "Hello Gorbo. I am tired of using scissors like some sort of boring ...person. I want to start a new trend. Wait for it... A CRAB! How interesting will it be to use a crab to cut things?! I can't wait. Can you make one?",
+        "Ooooh, Gorbo! Crabs are SO CUTE. Can you make me the CUTEST crab?",
     },
     {
-        "test squid 1",
-        "test squid 2",
+        "Beautiful morning, eh Gorbo? I keep running out of ink and I heard from Penelope that she uses a SQUID to get her ink. That sounds way easier than making it from berry juice every day. Can you copy me a squid?",
+        "Hi there. My aquarium is not complete without a squid. Do you do squids here?",
     },
     {
-        "test cactus 1",
-        "test cactus 2",
+        "Hi! These pesky birds won't stop eating my plants. The scarecrow I made isn't working, so I thought I'd show them by planting a cactus. So spiky! Can you copy a cactus for me?",
+        "Gorbo! Can't talk, I'm training for a marathon in the desert. I need to learn how to get water from a cactus so I need a cactus to practice. Can you make me one?",
     },
     {
-        "test boat 1",
-        "test boat 2",
+        "Siiiigh, hey Gorb. My nephew won't shut up about a pirate boat. Pirate boat, pirate boat, that's all he talks about. Could you make me a toy boat with that contraption of yours so maybe he'll stop talking about it?",
+        "Hello! My pet mice want to go on a seafaring adventure, I just know it. The first thing they need, aside from sailing knowledge, is a cool little boat. Could you make one of those?",
     },
     {
-        "test drumstick 1",
-        "test drumstick 2",
+        "Hi! Oh GOODNESS I'm so happy you're here. I'm so hungry, can you copy me some food? Anything, as long as it's a LOT. Maybe a drumstick or something?",
+        "Hey Gorbo! So, my husband wants to be a 'drummer', and he said that means he needs 'drumsticks'. I don't know why meat is so important for banging on a piece of wood but whatever. Can I have a turkey drumstick please?",
     },
     {
-        "test umbrella 1",
-        "test umbrella 2",
+        "Gorbo, what are you doing standing out in the rain?! Can you make me an umbrella? You might want to make yourself one too, if we're being honest.",
+        "Morning! I heard from my friend Goobily that if you have an umbrella, you can fly! Like, you can at LEAST fall really slowly. Could you copy one?",
     },
     {
-        "test cat 1",
-        "test cat 2",
-    },
+        "Ooooh, Gorbo, could I please have a kitty? Pretty please, I want a kitty so bad, I will name her Purrcilla and I will take SUCH good care of her!",
+        "Is this the copy place? I have such a mouse problem in my house, and I really need a cat to handle it. Maybe a cute one, please.",
+    }
 }
 
 local customerNames = {
-  "Esmerelda",
+  "Esmeralda",
   "Jiminy",
   "Rogerette",
   "Felicity",
